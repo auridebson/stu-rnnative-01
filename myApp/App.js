43fda2945215302ac9f6e5b1662762028916feff
Auridebson Lima <auridebson@vutus.com>
2024-03-17 12:08:41 -0300

    Update

--- myApp/App.js
+++ myApp/App.js
@@ -28,13 +28,13 @@ export default function App() {
 }
 
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+    margin: 10
   },
 });
